6a2443fc68af338f4a25039ef736eadc1b76acfc
Kyra Holmes <kyra@holmesmail.com>
2026-06-09 22:28:20 -0400

    Set up deployment CI/CD with github pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/kyraholmes.github.io/",
+  server: {
+    open: true,
+    port: 3001
+  }
 })
